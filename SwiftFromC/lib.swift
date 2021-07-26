0bf62bb976572e1ab2649785605ac1750d963fcc
smallville7123 <smallville7123@gmail.com>
2021-07-27 04:55:45 +1000

    remove unused bridging header

--- SwiftFromC/lib.swift
+++ SwiftFromC/lib.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-// see https://developer.apple.com/documentation/swift/imported_c_and_objective-c_apis/importing_objective-c_into_swift
+// if you want to call Objective C from Swift, see https://developer.apple.com/documentation/swift/imported_c_and_objective-c_apis/importing_objective-c_into_swift
 
 @objcMembers
 
