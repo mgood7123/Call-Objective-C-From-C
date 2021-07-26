234dd0cba1a1995df060807fb174de323476a27c
smallville7123 <smallville7123@gmail.com>
2021-07-27 02:54:13 +1000

    Initial Commit

---
+++ Call Objective C From C/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Call Objective C From C
+//
+//  Created by Matthew Good on 27/7/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
